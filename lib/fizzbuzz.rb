6f09a2d6bcbd555bf62415584f7fa763baca277f
emmassr <31829603+emmassr@users.noreply.github.com>
2018-05-04 13:49:24 +0100

    final commit

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,20 @@
 class Numbers
-	def one(number)
-		if number % 3 == 0
+	def one(number) # can use def self.one(number) instead of the subject = number.new i think??
+		if number % 15 == 0 
+			"FizzBuzz"
+		elsif number % 3 == 0
 			"Fizz"
+		elsif number % 5 == 0
+			"Buzz"	
 		else 
 			number
 		end	
 	end
+	
 end
 
 
+# use elseif loop for all methods
+# the number % 15 == 0 is first since the if the first condition is true it wont
+#run through the rest of the code. So if it was test whether the number was the 
+#multiple of 3 first and it was true it would run rest of the code. 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,7 +7,8 @@ describe Numbers do
 			expect(subject.one(1)) .to eql (1)
 		end
 	end
-
+#this block doesnt need to be a separate class since its linked to the numnbers class
+#we are teasting diff functions in the same class
 	describe ".one" do
 		it "returns Fizz" do
 			subject = Numbers.new
@@ -15,6 +16,20 @@ describe Numbers do
 		end
 	end
 	
+	describe ".one" do
+		it "returns Buzz" do
+			subject = Numbers.new
+			expect(subject.one(5)) .to eql ("Buzz")
+		end
+	end
+
+	describe ".one" do
+		it "returns Fizzbuzz" do
+			subject = Numbers.new
+			expect(subject.one(15)) .to eql ("FizzBuzz")
+		end
+	end
+
 end
 
 
